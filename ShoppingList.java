9c7a2dbb31daef1358c008f7f3470108c862af9b
joshepangel007-sketch <joshepangel007@gmail.com>
2026-03-20 18:30:18 -0500

    Primera practica con arraylistw

---
+++ ShoppingList.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        ArrayList<String> shoppingList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in); 
+
+        String productUser = "";  
+        String productRemove = ""; 
+        boolean search = false;
+        String searchProduct = "";
+
+        for (int i = 0; i <=4; i++) {
+            System.out.println("Dime el nombre del elemento que deseas agregar: ");
+            productUser = sc.nextLine(); 
+            shoppingList.add(productUser);
+        }
+
+        System.out.println("Tus productos son: " + shoppingList);
+
+        System.out.println("Dime el nommbre de el producto que deseas eliminar: ");
+        productRemove = sc.nextLine(); 
+        shoppingList.remove(productRemove);
+
+        System.out.println("Tu lista actual es de: ");
+
+        System.out.println("Dime el nombre de el prooducto a buscar");
+        searchProduct = sc.nextLine();
+
+        for (String item : shoppingList) {
+            if (item.equals(searchProduct)) {
+                System.out.println("Producto existente en la lista");
+                search = true;
+                break;
+            }
+        }
+
+        if (!search) {
+            System.out.println("el producto  no existe");
+        }
+    }    
+}
